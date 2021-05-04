7712a8f127a9a42314a81b870bcb0082bbe7cdce
Josema711 <14josemanuel@gmail.com>
2021-05-04 13:51:45 +0200

    Primera subida

---
+++ database/seeders/CochesSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CochesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+          DB::table('coches')->insert([
+       'modelo'=> 'M2',
+       'marca'=> 'BMW',
+       'cv'=>'390 CV',
+       'color'=>'Azul intenso',
+       'precio'=>'80.000€',
+            ]);
+          DB::table('coches')->insert([
+        'modelo'=> 'M8',
+        'marca'=> 'BMW',
+        'cv'=>'600 CV',
+        'color'=>'Negro mate',
+        'precio'=>'190.000€',
+            ]);
+          DB::table('coches')->insert([
+        'modelo'=> 'Urus',
+        'marca'=> 'Lamborghini',
+        'cv'=>'485 CV',
+        'color'=>'Amarillo',
+        'precio'=>'280.000€',
+            ]);
+    }
+}
